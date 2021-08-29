0b29d42424f406d4d3b95c624af341773b27de7e
abhijithdakarapu99 <abhijithdakarapu@gmail.com>
2021-08-29 21:05:28 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var choice = ["data Structures","writing","web"];
+var number = Math.floor(Math.random()*4);
+choice[number];
+
+
+function hippo(){
+
+
+    var choice = ["data Structures","writing","web"];
+    var number = Math.floor(Math.random()*3);
+    choice[number];
+    
+    }
